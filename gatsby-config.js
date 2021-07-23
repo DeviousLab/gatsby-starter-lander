133129ea80802d4c0732ba0a5c0b0449cfc6ad6a
DeviousLab <deviouslab@gmail.com>
2021-07-23 22:43:30 +0400

    Added favicon to site

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `Tanmu Project Management Services`,
-    description: `Tanmu Project Management Services and Consultancy in Dubai and the Middle East`,
+    description: `Tanmu Project Management Services and Consultancy in Dubai, United Arab Emirates`,
     author: `@DeviousLab`,
     keywords: `tanmu, project, management, services, engineering, construction, handover, consultant, dubai`
   },
@@ -11,6 +11,18 @@ module.exports = {
       options: {
         tailwindConfig: `tailwind.config.js`
       }
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'Tanmu Project Management Services',
+        short_name: 'Tanmu PMS',
+        start_url: '/',
+        background_color: '#ffffff',
+        theme_color: '#ff7b24',
+        display: 'standalone',
+        icon: 'src/images/logo.png',
+      },
     },
     'gatsby-plugin-react-helmet',
   ]
